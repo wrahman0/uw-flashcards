19433fc6f751cc807bd9f0d29b42723422663b77
Wasiur Rahman <wrahman0@gmail.com>
2016-12-18 19:03:29 -0500

    Added cs348 notes

--- quiz.cc
+++ quiz.cc
@@ -4,13 +4,22 @@
 #include <string>
 using namespace std;
 
-int main (int argc, char *argv) {
+void readFile(string *filename) {
+    cout << "Reading: " << filename << endl;
+
     ifstream quiz;
-    quiz.open (*argv[1]);
+    quiz.open(filename);
     string in;
+
     while ( quiz >> in ){
         cout << in << endl;
     }
+
     quiz.close();
+}
+
+int main (int argc, char **argv) {
+    cout << argv[1] << endl;
+    // if (argc >= 2) readFile(argv[1]);
     return 0;
 }
